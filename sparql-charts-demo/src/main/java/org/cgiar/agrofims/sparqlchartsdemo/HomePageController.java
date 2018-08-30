1f85707f21a3c9d599f262d7fe4d86761d7b63a3
Brian Lowe <brian@ontocale.com>
2018-08-30 12:55:48 +0300

    Initial commit of webapp

---
+++ sparql-charts-demo/src/main/java/org/cgiar/agrofims/sparqlchartsdemo/HomePageController.java
@@ -0,0 +1,20 @@
+package org.cgiar.agrofims.sparqlchartsdemo;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomePageController extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        request.setAttribute("body", "test message: x");
+        RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+        rd.forward(request, response);        
+    }
+    
+}
---
+++ sparql-charts-demo/src/main/webapp/home.jsp
@@ -0,0 +1,10 @@
+<html>
+
+<head>
+  <title>SPARQL Charts Demo</title>
+</head>
+
+<body>
+  <h1>SPARQL Charts Demo</h1>
+  <p>${body}</p>
+</body></html>
