08de0c5c8eea9d5f9b3a22bdcd425cd9356cabc0
Brian Lowe <brian@ontocale.com>
2018-08-30 17:58:01 +0300

    Add first two queries via resource files

--- sparql-charts-demo/src/main/java/org/cgiar/agrofims/sparqlchartsdemo/HomePageController.java
+++ sparql-charts-demo/src/main/java/org/cgiar/agrofims/sparqlchartsdemo/HomePageController.java
@@ -1,6 +1,9 @@
 package org.cgiar.agrofims.sparqlchartsdemo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,11 +13,39 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HomePageController extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
+    private static final String QUERIES_DIR = "/queries/";
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        request.setAttribute("body", "test message: x");
+        request.setAttribute("query1", loadResource(QUERIES_DIR + "query1.rq"));
+        request.setAttribute("query2", loadResource(QUERIES_DIR + "query2.rq"));
         RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
         rd.forward(request, response);        
     }
     
+    protected String loadResource(String resourcePath) {
+        InputStream inputStream = this.getClass().getResourceAsStream(
+                resourcePath);
+        StringBuffer fileContents = new StringBuffer();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String ln;
+            while ( (ln = reader.readLine()) != null) {
+                fileContents.append(ln).append('\n');
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load " + resourcePath, e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return fileContents.toString();
+    }
 }
--- sparql-charts-demo/src/main/webapp/home.jsp
+++ sparql-charts-demo/src/main/webapp/home.jsp
@@ -1,10 +1,42 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
 <html>
-
-<head>
-  <title>SPARQL Charts Demo</title>
-</head>
-
-<body>
-  <h1>SPARQL Charts Demo</h1>
-  <p>${body}</p>
-</body></html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="charts-from-csv/styles.css">
+    <title>SIMLESA Maize Yields</title>
+  </head>
+  <body>
+  
+    <h1>SIMLESA Maize Yields</h1>
+    
+    <h2>List of plots with maize yield</h2>
+    
+    <table><tr>
+    
+    <td><form method="GET" action="TODO">
+      <input type="submit" class="nav-btn" value="SPARQL">
+    </form></td>
+    
+    <td><form method="GET" action="http://35.156.71.103/vivo/admin/sparqlquery">
+      <input type="submit" class="nav-btn" value="Results"/>
+      <input type="hidden" name="resultFormat" value="text/plain"/>
+      <input type="hidden" name="query" value="${query1}"/>
+    </form></td>
+    
+    </tr></table>
+    
+    <h2>Maize yield by locality</h2>
+    
+    <table><tr>
+    
+    <td><form method="GET" action="charts-from-csv/index.html">
+      <input type="submit" class="nav-btn" value="SPARQL and Chart"/>
+      <input name="q" type="hidden" value="${query2}"/>
+    </form></td>
+    
+    </tr></table>
+    
+  </body>
+</html>
