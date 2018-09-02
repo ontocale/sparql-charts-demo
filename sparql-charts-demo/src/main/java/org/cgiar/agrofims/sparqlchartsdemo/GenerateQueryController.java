441310f30bb81101371373b0c65e32bb8ac852e0
Brian Lowe <brian@ontocale.com>
2018-09-02 16:08:37 +0300

    Add user-modifiable query

--- sparql-charts-demo/src/main/java/org/cgiar/agrofims/sparqlchartsdemo/GenerateQueryController.java
+++ sparql-charts-demo/src/main/java/org/cgiar/agrofims/sparqlchartsdemo/GenerateQueryController.java
@@ -10,9 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.http.client.utils.URIBuilder;
 
 public class GenerateQueryController extends HomePageController {
-
-    // TODO add query resource file with replaceable parameters
-    // build query based on submitted URL parameters
    
     private static final long serialVersionUID = 1L;
 
@@ -21,14 +18,60 @@ public class GenerateQueryController extends HomePageController {
         String condition1a = request.getParameter("condition1a");
         String condition1b = request.getParameter("condition1b");
         String condition2a = request.getParameter("condition2a");
-        String condition2b = request.getParameter("condition2b");        
+        String condition2b = request.getParameter("condition2b");        ;
+        String q = loadResource(QUERIES_DIR + "queryBase.rq");         
+        q = bind(q, "column1Label", columnLabel(condition1a) + columnLabel(condition1b));
+        q = bind(q, "column2Label", columnLabel(condition2a) + columnLabel(condition2b));
+        q = bind(q, "oneProcessDetails", processDetails(condition1a, "oneA") + processDetailsOrder2(condition1b, "oneB"));
+        q = bind(q, "twoProcessDetails", processDetails(condition2a, "twoA") + processDetailsOrder2(condition2b, "twoB"));
         try {
-            URIBuilder redirectBuilder = new URIBuilder("charts-from-csv/index.html");
-            redirectBuilder.addParameter("q", condition1a + "\n" + condition1b + "\n" + condition2a + "\n" + condition2b);
+            URIBuilder redirectBuilder = new URIBuilder("charts-from-csv/index.html");            
+            redirectBuilder.addParameter("q", q);
             response.sendRedirect(redirectBuilder.build().toString());
         } catch (URISyntaxException e) {
             throw new ServletException(e);
         }
+    }
+    
+    String columnLabel(String condition) {
+        String[] parts = condition.split("\\|");
+        String salientPart;
+        if(!parts[3].isEmpty()) {
+            salientPart = parts[3];
+        } else {            
+            salientPart = parts[1];
+        }
+        return salientPart.replaceAll("\\W", "");
+    }
+    
+    String processDetails(String condition, String prefix) {
+        StringBuilder details = new StringBuilder();
+        String[] parts = condition.split("\\|");
+        details.append("  ").append("?").append(prefix).append("Process a <").append(parts[0]).append("> . \n");        
+        if(!parts[2].isEmpty()) {
+            details.append("  ?").append(prefix).append("participant a <").append(parts[2]).append("> . \n");
+            details.append("  ").append("?").append(prefix).append("Process obo:RO_0000057 ?").append(prefix).append("participant . \n");
+        }
+        details.append("  ?").append(prefix).append("Process obo:BFO_0000066 ?plot . \n");
+        return details.toString();
+    }
+
+    // Funky query optimization
+    String processDetailsOrder2(String condition, String prefix) {
+        StringBuilder details = new StringBuilder();
+        String[] parts = condition.split("\\|");
+        details.append("  ?").append(prefix).append("Process obo:BFO_0000066 ?plot . \n");        
+        if(!parts[2].isEmpty()) {
+            details.append("  ").append("?").append(prefix).append("Process obo:RO_0000057 ?").append(prefix).append("participant . \n");
+            details.append("  ?").append(prefix).append("participant a <").append(parts[2]).append("> . \n");
+        }
+        details.append("  ").append("?").append(prefix).append("Process a <").append(parts[0]).append("> . \n");
+        return details.toString();
+    }
+
+    
+    private String bind(String origString, String varName, String value) {
+        return origString.replaceAll("\\$\\{" + varName + "\\}", value);
     }
     
 }
